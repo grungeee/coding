972c6485c0eef28cb1fd8203c0a704ada93bf7c4
grungeee <sharapanikita@gmail.com>
2026-06-19 11:52:04 +0200

    Update ESP32-C3 IR chain button sketch

--- arduino_stuff/uno4_IRT/esp32c3_ir_chain_button/esp32c3_ir_chain_button.ino
+++ arduino_stuff/uno4_IRT/esp32c3_ir_chain_button/esp32c3_ir_chain_button.ino
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <IRremote.hpp>
+#include <esp32-hal-bt.h>
+#include <esp_sleep.h>
+#include <esp_wifi.h>
 
 namespace {
 constexpr uint8_t kIrLedPin = 4;
@@ -17,12 +20,17 @@ constexpr uint32_t kDebounceMs = 40;
 constexpr uint16_t kInterCommandDelayMs = 35;
 constexpr uint16_t kStepRepeats = 0;
 
-bool lastRawButtonState = HIGH;
-bool debouncedButtonState = HIGH;
-uint32_t lastDebounceTime = 0;
-
 void setStatusLed(bool enabled) {
   digitalWrite(kStatusLedPin, enabled ? HIGH : LOW);
+}
+
+void disableRadios() {
+  esp_wifi_stop();
+  esp_wifi_deinit();
+
+#if SOC_BT_SUPPORTED
+  btStop();
+#endif
 }
 
 void sendCommand(uint16_t command, uint8_t count, const char *label) {
@@ -49,6 +57,46 @@ void sendSequence() {
   sendCommand(kCmdSpeedUp, 29, "Speed Up");
   Serial.println("Sequence done.");
 }
+
+bool buttonIsPressed() {
+  return digitalRead(kButtonPin) == kButtonActive;
+}
+
+bool buttonIsPressedAfterDebounce() {
+  if (!buttonIsPressed()) {
+    return false;
+  }
+
+  delay(kDebounceMs);
+  return buttonIsPressed();
+}
+
+void waitForButtonRelease() {
+  while (buttonIsPressed()) {
+    delay(10);
+  }
+
+  delay(kDebounceMs);
+}
+
+void enterDeepSleep() {
+  setStatusLed(false);
+  pinMode(kIrLedPin, OUTPUT);
+  digitalWrite(kIrLedPin, LOW);
+
+  disableRadios();
+
+  const uint64_t wakePinMask = 1ULL << kButtonPin;
+  esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
+  esp_deep_sleep_enable_gpio_wakeup(wakePinMask, ESP_GPIO_WAKEUP_GPIO_LOW);
+#if SOC_PM_SUPPORT_MODEM_PD
+  esp_sleep_pd_config(ESP_PD_DOMAIN_MODEM, ESP_PD_OPTION_OFF);
+#endif
+
+  Serial.println("Entering deep sleep. Press button to wake and send.");
+  Serial.flush();
+  esp_deep_sleep_start();
+}
 }  // namespace
 
 void setup() {
@@ -63,25 +111,15 @@ void setup() {
   Serial.println("KY-005 signal pin: GPIO4");
   Serial.println("Button pin: GPIO3 to GND");
   Serial.println("Status LED pin: GPIO8");
+
+  const esp_sleep_wakeup_cause_t wakeupCause = esp_sleep_get_wakeup_cause();
+  if (wakeupCause == ESP_SLEEP_WAKEUP_GPIO || buttonIsPressedAfterDebounce()) {
+    sendSequence();
+    waitForButtonRelease();
+  }
+
+  enterDeepSleep();
 }
 
 void loop() {
-  const bool rawState = digitalRead(kButtonPin);
-
-  if (rawState != lastRawButtonState) {
-    lastDebounceTime = millis();
-    lastRawButtonState = rawState;
-  }
-
-  if ((millis() - lastDebounceTime) > kDebounceMs && rawState != debouncedButtonState) {
-    debouncedButtonState = rawState;
-
-    if (debouncedButtonState == kButtonActive) {
-      sendSequence();
-
-      while (digitalRead(kButtonPin) == kButtonActive) {
-        delay(10);
-      }
-    }
-  }
 }
